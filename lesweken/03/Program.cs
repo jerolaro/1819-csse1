ccbfcc662cf54ea4c695aa3a97ec620051d99be4
Jérôme Laroy <jerome@MacBook-Pro-van-Jerome.local>
2018-10-20 12:54:22 +0200

    opdracht O2

---
+++ lesweken/03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lesweek03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Het afdrukken van tekst in de console
+            Console.WriteLine("Simon");
+
+            // Rekenmachine
+            int a = 5;
+            int b = 6;
+            int resultaat1 = optellen(a, b);
+            int resultaat2 = deling(a, b);
+            int resultaat3 = verhogen(a);
+
+            Console.WriteLine(optellen(a, b));
+            Console.WriteLine(deling(a, b));
+            Console.WriteLine(verhogen(a));
+        }
+
+        static int optellen(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int deling(int a, int b)
+        {
+            return a/b;
+        }
+
+        static int verhogen(int a)
+        {
+            return a++;
+        }
+    }
+}
---
+++ opdrachten/02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Factulteit
+            int getal = 10;
+            int fac = getal;
+
+            while(getal > 2) {
+                getal--;
+                fac *= getal;
+            }
+
+            Console.WriteLine(fac);
+            
+            
+            // Fibonacci
+            int g0 = 0;
+            int g1 = 1;
+            int next = 0;
+
+            for(int i = 0; i < 15; i++) {
+                if(i == 0) {
+                    next = 0;
+                } else if(i == 1) {
+                    next = 1;
+                } else {
+                    next = g0 + g1;
+                    g0 = g1;
+                    g1 = next;
+                }
+                Console.WriteLine(next);
+            }
+        }
+    }
+}
